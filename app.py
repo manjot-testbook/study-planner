bfc59a23c21d9f1197114f936714387d3f2595cc
manjot-testbook <134503379+manjot-testbook@users.noreply.github.com>
2024-06-19 12:34:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,13 +15,13 @@ selected_option_4 = st.selectbox("Have you appreared for UPSC:", options_set_3)
 selected_option_5 = st.selectbox("Are you familiar with UPSC Curriculum", options_set_3)
 
 pdf_mapping = {
-    ("Full time Aspirant", "2 - 4 hours", "Yes"): "https://github.com/mickee00000/201951090_Research_Internship_2022/raw/main/Report%20&%20Presentation/Project_Report_Summer%20Research%20Intership%202022%20.pdf",
+    ("Option 1.1", "Option 2.1", "Option 3.1"): "https://github.com/mickee00000/201951090_Research_Internship_2022/raw/main/Report%20&%20Presentation/Project_Report_Summer%20Research%20Intership%202022%20.pdf",
     ("Option 1.1", "Option 2.1", "Option 3.2"): "https://github.com/mickee00000/201951090_Research_Internship_2022/raw/main/Report%20&%20Presentation/Project_Report_Summer%20Research%20Intership%202022%20.pdf",
     ("Option 1.1", "Option 2.2", "Option 3.3"): "https://github.com/mickee00000/201951090_Research_Internship_2022/raw/main/Report%20&%20Presentation/Project_Report_Summer%20Research%20Intership%202022%20.pdf",
     ("Option 1.2", "Option 2.3", "Option 3.4"): "https://github.com/mickee00000/201951090_Research_Internship_2022/raw/main/Report%20&%20Presentation/Project_Report_Summer%20Research%20Intership%202022%20.pdf"
 }
 
-pdf_url = pdf_mapping.get((selected_option_1, selected_option_2, selected_option_3))
+pdf_url = pdf_mapping.get((selected_option_1, selected_option_2, selected_option_3, selected_option_4))
 
 if (selected_option_1, selected_option_2, selected_option_3, selected_option_4) == ("Select an Option","Select an Option","Select an Option"):
     st.write("Select the listed options above to generate a PDF")
